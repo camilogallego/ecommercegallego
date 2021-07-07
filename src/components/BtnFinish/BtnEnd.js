00f374c9ad70380b5dd5038ac111e61468b96986
camilo <camilogallego88@hotmail.com>
2021-07-06 21:55:47 -0300

    add button delete and conditional rendering

--- src/components/BtnFinish/BtnEnd.js
+++ src/components/BtnFinish/BtnEnd.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './BtnEnd.css'
 
-function BtnAdd() {
+function BtnEnd() {
     return (
         <div>
             <Link to={"/Cart"}>
-            <button className="btnAdd">Finalizar Compra</button>
+            <button className="BtnEnd">Finalizar Compra</button>
             </Link>
         </div>
     )
 }
 
-export default BtnAdd
+export default BtnEnd
